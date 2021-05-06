8ee221938859b0eaa4c088b1c7723a37e098bbac
MarianaAya <umemuramariana@gmail.com>
2021-05-06 10:43:57 -0300

    alteração no controller

--- src/Controllers/FecharCancelarServicoController.js
+++ src/Controllers/FecharCancelarServicoController.js
@@ -43,6 +43,7 @@ module.exports={
         while(i<servico.getContas().length && servico.getContas()[i].getDtPgto()==null)
             i++;
         if(i===servico.getContas().length){
+        
             servico.getContas()[0].deletarContaServico(db);
             servico.setFim(null);
             servico.setStatus(true);
