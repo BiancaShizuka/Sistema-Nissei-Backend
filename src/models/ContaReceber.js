796075ead8552a2412095f7a7e52a11bd0771d69
MarianaAya <umemuramariana@gmail.com>
2021-05-15 18:49:10 -0300

    filtro por nome do cliente na conta a receber

--- src/models/ContaReceber.js
+++ src/models/ContaReceber.js
@@ -1,8 +1,7 @@
 const ContaReceberDAO=require('../DAOs/ContaReceberDAO');
 module.exports=class ContaReceber{
-    constructor(con_cod,ser_cod,con_valor,con_dtVencimento,con_dtPgto){
+    constructor(con_cod,con_valor,con_dtVencimento,con_dtPgto){
         this.con_cod=con_cod;
-        this.ser_cod=ser_cod;
         this.con_valor=con_valor;
         this.con_dtVencimento=con_dtVencimento;
         this.con_dtPgto=con_dtPgto;
@@ -17,29 +16,27 @@ module.exports=class ContaReceber{
     getDtVenc(){
         return this.con_dtVencimento
     }
-    getSerCod(){
-        return this.ser_cod;
-    }
+
     setDtPgto(data){
         this.con_dtPgto=data;
     }
     getDtPgto(){
         return this.con_dtPgto;
     }
-    async gravar(db){
-        let result = await new ContaReceberDAO().gravar(this,db);
+    async gravar(ser_cod,db){
+        let result = await new ContaReceberDAO().gravar(this,ser_cod,db);
 
     }
-    async alterar(db){
+    async alterar(ser_cod,db){
    
-        let result = await new ContaReceberDAO().alterar(this,db);
+        let result = await new ContaReceberDAO().alterar(this,ser_cod,db);
         return result;
     }
     
     async getConta(con_cod,ser_cod,db){
 
         let resp= await new ContaReceberDAO().consultar(con_cod,ser_cod,db);
-        return new ContaReceber(resp.data[0].con_cod,resp.data[0].ser_cod,resp.data[0].con_valor,resp.data[0].con_dtVencimento,resp.data[0].con_dtPgto)
+        return new ContaReceber(resp.data[0].con_cod,resp.data[0].con_valor,resp.data[0].con_dtVencimento,resp.data[0].con_dtPgto)
     }
     async deletarContaServico(ser_cod,db){
    
@@ -50,21 +47,23 @@ module.exports=class ContaReceber{
         let contas=[];
         for(let i=0;i<resp.data.length;i++){
             contas.push(
-                new ContaReceber(resp.data[i].con_cod,resp.data[i].ser_cod,resp.data[i].con_valor,resp.data[i].con_dtVencimento,resp.data[i].con_dtPgto)
+                new ContaReceber(resp.data[i].con_cod,resp.data[i].con_valor,resp.data[i].con_dtVencimento,resp.data[i].con_dtPgto)
                 
             )
 
         }
         return contas;
     }
-    async listarContasFiltro(dtInicio,dtFim,status,db){
-        let resp= await new ContaReceberDAO().consultarContasFiltro(dtInicio,dtFim,status,db);
+    async listarContasFiltro(dtInicio,dtFim,status,cliente,db){
+        let resp= await new ContaReceberDAO().consultarContasFiltro(dtInicio,dtFim,status,cliente,db);
         let contas=[];
+       
         for(let i=0;i<resp.data.length;i++){
-            contas.push(
-                new ContaReceber(resp.data[i].con_cod,resp.data[i].ser_cod,resp.data[i].con_valor,resp.data[i].con_dtVencimento,resp.data[i].con_dtPgto)
-                
-            )
+            let c=new ContaReceber(resp.data[i].con_cod,resp.data[i].con_valor,resp.data[i].con_dtVencimento,resp.data[i].con_dtPgto);
+            c['ser_cod']=resp.data[i].ser_cod;
+            c['cli_nome']=resp.data[i].pes_nome;
+ 
+            contas.push(c)
         }
         return contas;
     }
