44438f23b828ad7322f3fa7f8cdd5e7ba229abf8
Bianca <umemurabianca@gmail.com>
2021-08-27 15:35:09 -0300

    funcao pagarParcial

--- src/routes.js
+++ src/routes.js
@@ -77,7 +77,6 @@ routes.delete('/servico/:cod',servicoCtrl.excluir);
 
 const contaReceberCtr=require('./Controllers/EfetuarRecebimentoController');
 routes.put('/contaReceber',contaReceberCtr.alterar);
-routes.put('/contaReceberParcial',contaReceberCtr.receberParcial);
 routes.get('/contaReceberFiltros',contaReceberCtr.listarContasFiltro);
 
 const fecharServicoCtr=require('./Controllers/FecharServicoController');
